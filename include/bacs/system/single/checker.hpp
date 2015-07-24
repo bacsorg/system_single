6189c2a0b4d3fdcd2d9009057a8246337154b572
Aleksey Filippov <sarum9in@gmail.com>
2015-07-24 19:58:20 +0400

    Added checker.

---
+++ include/bacs/system/single/checker.hpp
@@ -0,0 +1,49 @@
+#pragma once
+
+#include <bacs/problem/single/result.pb.h>
+
+#include <yandex/contest/invoker/Forward.hpp>
+
+#include <bunsan/plugin.hpp>
+
+#include <boost/filesystem/path.hpp>
+#include <boost/noncopyable.hpp>
+#include <boost/optional.hpp>
+
+#include <memory>
+#include <string>
+#include <unordered_map>
+
+namespace bacs {
+namespace system {
+namespace single {
+
+class checker : private boost::noncopyable {
+ public:
+  class result_mapper : private boost::noncopyable {
+    BUNSAN_PLUGIN_AUTO_BODY(result_mapper)
+   public:
+    virtual ~result_mapper() {}
+
+    virtual problem::single::result::Judge::Status map(int exit_status) = 0;
+  };
+  BUNSAN_PLUGIN_TYPES(result_mapper)
+
+ private:
+  BUNSAN_PLUGIN_AUTO_BODY(
+      checker, const yandex::contest::invoker::ContainerPointer &container,
+      result_mapper_uptr mapper)
+
+ public:
+  using file_map = std::unordered_map<std::string, boost::filesystem::path>;
+
+  virtual ~checker() {}
+
+  virtual bool check(const file_map &test_files, const file_map &solution_files,
+                     problem::single::result::Judge &result) = 0;
+};
+BUNSAN_PLUGIN_TYPES(checker)
+
+}  // namespace single
+}  // namespace system
+}  // namespace bacs
